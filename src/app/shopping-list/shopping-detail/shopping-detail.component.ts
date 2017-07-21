b0cbf4cff801eaef94609cd599eacb5775db1f9b
X852591 <evzepedaj@santander.com.mx>
2017-07-21 13:43:48 -0500

    add component recipies

---
+++ src/app/shopping-list/shopping-detail/shopping-detail.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-shopping-detail',
+  templateUrl: './shopping-detail.component.html',
+  styleUrls: ['./shopping-detail.component.css']
+})
+export class ShoppingDetailComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
