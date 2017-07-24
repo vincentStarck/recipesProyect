aef746919fdb46cf18d827dabc3326f1070d030b
vincentStarck <vincent.starck@gmail.com>
2017-07-23 23:23:19 -0500

    update

--- src/app/recipes/recipe-list/recipe-list.component.ts
+++ src/app/recipes/recipe-list/recipe-list.component.ts
@@ -8,7 +8,10 @@ import { Recipe } from "../recipe.model";
 })
 export class RecipeListComponent implements OnInit {
   recipes:Recipe[]=[
+    new Recipe("A test recipe","This is simply a test ","http://www.seriouseats.com/images/2015/09/20150914-pressure-cooker-recipes-roundup-09.jpg"),
     new Recipe("A test recipe","This is simply a test ","http://www.seriouseats.com/images/2015/09/20150914-pressure-cooker-recipes-roundup-09.jpg")
+    
+
 
   ];
 
